7fa41208f9eefde6ee15b39cf67eba8dfcf98853
zhouzhou <1783893088@qq.com>
2023-02-03 11:12:44 +0800

    利用vuex-persistedstate@3.2.1插件进行本地存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,26 @@
 // 持久化存储
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  // 配置 token
+  // 配置 token 字段
   state: {
     token: ''
   },
   getters: {},
   mutations: {
+    // 配置 更新 token 的方法
     updateToken(state, val) {
       state.token = val
     }
   },
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [
+    // 配置 vuex 持久化存储的插件
+    createPersistedState()
+  ]
 })
