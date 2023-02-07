d3b6880c9268c5b805a0f88867d6c95278297f4b
zhouzhou <1783893088@qq.com>
2023-02-07 10:44:42 +0800

    全局守卫设置跳转权限

--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,26 @@ const router = new VueRouter({
   routes
 })
 
+// 不需要登陆即可访问
+const whiteList = ['/login', '/reg']
+
 // 全局路由守卫
 router.beforeEach((to, from, next) => {
   const token = store.state.token
   // 本地有 token 去存用户信息进到vuex
-  if (token && !store.state.userinfo.username) {
-    store.dispatch('getUserinfoActions')
+  if (token) {
+    // 放行所有
+    if (!store.state.userinfo.username) {
+      store.dispatch('getUserinfoActions')
+    }
+    next()
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
   }
-  next()
 })
 
 export default router
