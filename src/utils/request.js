2325ac2e4aa838a1d0bcbcd513d31b2eceb96d4a
zhouzhou <1783893088@qq.com>
2023-02-07 11:21:21 +0800

    配置响应拦截器，身份过期跳转登陆

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,9 @@
 // 封装 axios 请求
 // 步骤1：获取 axios 实例
+import router from '@/router'
 import store from '@/store'
 import axios from 'axios'
+import { Message } from 'element-ui'
 
 // 设置 api 请求根路径
 const myAxios = axios.create({
@@ -19,6 +21,28 @@ myAxios.interceptors.request.use(
   },
   function (error) {
     // 对请求错误做些什么
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+myAxios.interceptors.response.use(
+  function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response
+  },
+  function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    if (error.response.status === 401) {
+      store.commit('updateToken', '')
+      store.commit('updateUserinfo', {})
+
+      router.push('/login')
+
+      Message.error('登陆验证已经过期！！')
+    }
     return Promise.reject(error)
   }
 )
